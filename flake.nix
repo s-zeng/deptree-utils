6fd0de8c1ee8396160768641024a8017b4295123
Simon Zeng <contact@simonzeng.com>
2025-11-21 17:54:52 +0000

    Revert to old-style flake.nix
    I'm sorry srid, but your latest template repo just does too much. I
    wanted cargo v1.88 but was stuck on v1.84. Couldn't even get v1.85 which
    has stable 2024 edition...

--- flake.nix
+++ flake.nix
@@ -1,26 +1,77 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
-    rust-flake.url = "github:juspay/rust-flake";
-    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    cargo-doc-live.url = "github:srid/cargo-doc-live";
 
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.flake = false;
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { config, self', pkgs, lib, system, ... }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          nonRustDeps = [
+            pkgs.libiconv
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.pandoc
+            pkgs.texlive.combined.scheme-small
+          ];
+        in
+        {
+          # Rust package
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            inherit (cargoToml.package) name version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = nonRustDeps;
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            LIBRARY_PATH = "${pkgs.libiconv}/lib";
+          };
 
-      # See ./nix/modules/*.nix for the modules that are imported here.
-      imports = with builtins;
-        map
-          (fn: ./nix/modules/${fn})
-          (attrNames (readDir ./nix/modules));
+          # Rust dev environment
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+            ];
+            shellHook = ''
+              # For rust-analyzer 'hover' tooltips to work.
+              export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+              # openssl
+              export OPENSSL_DIR="${pkgs.openssl.dev}"
+              export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+              export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
+              export OPENSSL_DEV="${pkgs.openssl.dev}"
+              # libiconv
+              export LIBRARY_PATH="${pkgs.libiconv}/lib:$LIBRARY_PATH"
+            '';
+            buildInputs = nonRustDeps;
+            nativeBuildInputs = with pkgs; [
+              just
+              rustc
+              cargo
+              cargo-watch
+              cargo-insta
+              rust-analyzer
+            ];
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+        };
     };
 }
